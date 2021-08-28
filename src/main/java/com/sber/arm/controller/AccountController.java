0d141dc029b478ec7d86c2191a23811b033179f9
Stepan Kononov <kononovsn@hotmail.com>
2021-08-29 01:37:17 +0300

    + add transaction

--- src/main/java/com/sber/arm/controller/AccountController.java
+++ src/main/java/com/sber/arm/controller/AccountController.java
@@ -1,7 +1,11 @@
 package com.sber.arm.controller;
 
 import com.sber.arm.entity.Account;
+import com.sber.arm.entity.Client;
+import com.sber.arm.entity.Transaction;
 import com.sber.arm.repository.AccountRepository;
+import com.sber.arm.repository.ClientRepository;
+import com.sber.arm.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
@@ -15,11 +19,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class AccountController {
     @Autowired
     AccountRepository accountRepository;
+    @Autowired
+    TransactionRepository transactionRepository;
+    @Autowired
+    ClientRepository clientRepository;
 
     @GetMapping("/account/add/{id}")
     public String accountAdd(@PathVariable(value = "id") int clientId, Model model){
@@ -35,5 +45,18 @@ public class AccountController {
         Account account = new Account(number, LocalDateTime.now(), balance, owner);
         accountRepository.save(account);
         return "redirect:/client/" + clientId;
+    }
+    @GetMapping("/account/{id}")
+    public String accountView(@PathVariable(value = "id") int accountId, Model model){
+        Optional<Account> account = accountRepository.findById(accountId);
+        List<Transaction> credit = transactionRepository.findAllByCreditAccount(account.get().getNumber());
+        List<Transaction> debit = transactionRepository.findAllByDebitAccount(account.get().getNumber());
+        Optional<Client> optionalClient = clientRepository.findById(account.get().getOwner());
+        model.addAttribute("title","счет");
+        model.addAttribute("account",account.get());
+        model.addAttribute("owner", optionalClient.get());
+        model.addAttribute("credit",credit);
+        model.addAttribute("debit",debit);
+        return "account";
     }
 }
--- src/main/java/com/sber/arm/repository/TransactionRepository.java
+++ src/main/java/com/sber/arm/repository/TransactionRepository.java
@@ -3,5 +3,9 @@ package com.sber.arm.repository;
 import com.sber.arm.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<Transaction,Integer> {
+    List<Transaction> findAllByCreditAccount(String creditAccount);
+    List<Transaction> findAllByDebitAccount(String debitAccount);
 }
