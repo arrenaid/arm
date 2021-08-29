31f407bd9acf12cd12b06d1c16018d8ae9fc51ee
Stepan Kononov <kononovsn@hotmail.com>
2021-08-30 00:18:02 +0300

    + add security

---
+++ src/main/java/com/sber/arm/config/WebSecurityConfig.java
@@ -0,0 +1,56 @@
+package com.sber.arm.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+//                .antMatchers("/", "/home").permitAll()
+                .antMatchers().permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+        UserDetails user2 =
+                User.withDefaultPasswordEncoder()
+                        .username("u")
+                        .password("p")
+                        .roles("USER")
+                        .build();
+        UserDetails user3 =
+                User.withDefaultPasswordEncoder()
+                        .username("admin")
+                        .password("root")
+                        .roles("USER")
+                        .build();
+        return new InMemoryUserDetailsManager(user,user2,user3);
+    }
+}
